98ca70d46481021277e7da1c572a049f1b6ca80b
Anatolii Zatsarnyi <vip.zatsarnyy@gmail.com>
2022-02-15 06:25:58 +0200

    new

--- app/js/main.js
+++ app/js/main.js
@@ -1,38 +1,9 @@
 $(function () {
   const swiper = new Swiper('.swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.portfolio__button-next',
+      prevEl: '.portfolio__button-prev',
     },
-
-    spaceBetween: 30,
-    slidesPerView: 1,
-
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
-  $('.header__nav-item a').on('click', function () {
-
-    let href = $(this).attr('href');
-
-    $('html, body').animate({
-      scrollTop: $(href).offset().top
-    }, {
-      duration: 370,
-      easing: "linear"
-    });
-
-    return false;
-  });
-
-  $(".header__nav-link").on("click", function () {
-    $(".header__nav-link").removeClass("active");
-    $(this).addClass("active");
   });
 });
